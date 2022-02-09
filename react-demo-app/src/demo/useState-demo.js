23f6f8ecf08a4e39a2c27f09326285fe6f571422
fengduoduo <fengduoduo@njsdata.com>
2022-02-09 14:53:22 +0800

    feat: useReduce,useContext

--- react-demo-app/src/demo/useState-demo.js
+++ react-demo-app/src/demo/useState-demo.js
@@ -70,43 +70,96 @@
 //   );
 // }
 // export default Counter2;
-import React, { useState, memo, useMemo, useCallback } from "react";
+// import React, { useState, memo, useMemo, useCallback } from "react";
 
-function SubCounter({ onClick }) {
-  console.log("SubCounter render");
-  return <button onClick={onClick}>111</button>;
-}
-SubCounter = memo(SubCounter);
+// function SubCounter({ onClick }) {
+//   console.log("SubCounter render");
+//   return <button onClick={onClick}>111</button>;
+// }
+// SubCounter = memo(SubCounter);
 
-let oldData, oldAddClick;
-export default function Counter2() {
-  console.log("Counter render");
-  const [name, setName] = useState("计数器");
-  const [number, setNumber] = useState(0);
-  // 父组件更新时，这里的变量和函数每次都会重新创建，那么子组件接受到的属性每次都会认为是新的
-  // 所以子组件也会随之更新，这时候可以用到 useMemo
-  // 有没有后面的依赖项数组很重要，否则还是会重新渲染
-  // 如果后面的依赖项数组没有值的话，即使父组件的 number 值改变了，子组件也不会去更新
-  //const data = useMemo(()=>({number}),[]);
-  const data = useMemo(() => ({ number }), [number]);
-  console.log("data===oldData ", data === oldData);
-  oldData = data;
+// let oldData, oldAddClick;
+// export default function Counter2() {
+//   console.log("Counter render");
+//   const [name, setName] = useState("计数器");
+//   const [number, setNumber] = useState(0);
+//   // 父组件更新时，这里的变量和函数每次都会重新创建，那么子组件接受到的属性每次都会认为是新的
+//   // 所以子组件也会随之更新，这时候可以用到 useMemo
+//   // 有没有后面的依赖项数组很重要，否则还是会重新渲染
+//   // 如果后面的依赖项数组没有值的话，即使父组件的 number 值改变了，子组件也不会去更新
+//   //const data = useMemo(()=>({number}),[]);
+//   const data = useMemo(() => ({ number }), [number]);
+//   console.log("data===oldData ", data === oldData);
+//   oldData = data;
 
-  // 有没有后面的依赖项数组很重要，否则还是会重新渲染
-  const addClick = useCallback(() => {
-    setNumber(number + 1);
-  }, []);
-  console.log("addClick===oldAddClick ", addClick === oldAddClick);
-  oldAddClick = addClick;
+//   // 有没有后面的依赖项数组很重要，否则还是会重新渲染
+//   const addClick = useCallback(() => {
+//     setNumber(number + 1);
+//   }, []);
+//   console.log("addClick===oldAddClick ", addClick === oldAddClick);
+//   oldAddClick = addClick;
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//       />
+//       <div>{number}</div>
+//       <SubCounter onClick={addClick} />
+//     </>
+//   );
+// }
+import React, { useReducer, createContext, useContext } from "react";
+const initialState = 0;
+const reduce = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { number: state.number + 1 };
+    case "decrement":
+      return { number: state.number - 1 };
+    default:
+      throw new Error();
+  }
+};
+const init = (initialState) => {
+  return { number: initialState };
+};
+const CounterContext = createContext();
+
+const SubCounter_one = () => {
+  return (
+    <CounterContext.Consumer>
+      {({ state, dispatch }) => (
+        <>
+          <div>{state.number}</div>
+          <button onClick={() => dispatch({ type: "increment" })}>+++</button>
+          <button onClick={() => dispatch({ type: "decrement" })}>---</button>
+        </>
+      )}
+    </CounterContext.Consumer>
+  );
+};
+
+const SubCounter_two = () => {
+  const { state, dispatch } = useContext(CounterContext);
   return (
     <>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <div>{number}</div>
-      <SubCounter onClick={addClick} />
+      <div>{state.number}</div>
+      <button onClick={() => dispatch({ type: "increment" })}>+++</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>---</button>
     </>
   );
-}
+};
+const Counter = () => {
+  const [state, dispatch] = useReducer(reduce, initialState, init);
+
+  return (
+    <div>
+      <CounterContext.Provider value={{ state, dispatch }}>
+        <SubCounter_one />
+      </CounterContext.Provider>
+    </div>
+  );
+};
+export default Counter;
